75cf3ab4caabb6a2478e78104392ab4a66f5d1e6
Omerrizabahadir <omrbahadir@gmail.com>
2024-12-03 17:38:24 +0300

    Listing the given array first with Arrays.asList and reversing it with Collections.reverse

---
+++ src/arrays/ReversedArrayWithCollections.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ReversedArrayWithCollections {
+    public static void main(String[] args) {
+        Integer[] numbers = {1, 2, 3, 4, 5};
+
+        //önce listeye çevirme
+        List<Integer> listNumbers = Arrays.asList(numbers);
+
+        //collections ile terse çevirme
+        Collections.reverse(listNumbers);
+
+        for (int num : listNumbers){
+            System.out.println(num);
+        }
+    }
+}
