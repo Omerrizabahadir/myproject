0e1fc35391e748c662be340b62eeadbc2a7e85b2
Omerrizabahadir <omrbahadir@gmail.com>
2024-12-03 13:52:40 +0300

    solution of are these numbers equal?

---
+++ src/com/edabit_solutions/methods/AreNumbersEqual.java
@@ -0,0 +1,26 @@
+package com.edabit_solutions.methods;
+
+/*Create a function that returns true when x is equal to y; otherwise return false.
+    Examples
+    isSameNum(4, 8) ➞ false
+
+    isSameNum(2, 2) ➞  true
+
+    isSameNum(42, 32) ➞  false
+    Notes
+    Don't forget to return the result.*/
+public class AreNumbersEqual {
+
+
+    public static void main(String[] args) {
+       int x=4;
+       int y =3;
+
+       if (x==y){
+           System.out.println(true);
+       }else {
+           System.out.println(false);
+       }
+    }
+
+}
