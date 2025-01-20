2857f2d486ab72aedc8b02d1c9a1687e0283e03d
Omerrizabahadir <omrbahadir@gmail.com>
2025-01-20 14:10:12 +0300

    HackerRank solution of Sams Grades Problrm

---
+++ src/hackerrank_solutions/SamGrades1.java
@@ -0,0 +1,24 @@
+package hackerrank_solutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SamGrades1 {
+
+    public static List<Integer> gradingStudents(List<Integer> grades){
+        List <Integer> result = new ArrayList<>();
+        for(int grade : grades){
+          if(grade < 38 || grade %5 <3){
+              result.add(grade);
+          }else{
+              result.add(grade +(5-grade % 5));
+          }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        List<Integer> grades = Arrays.asList(73,67,38,33);
+        System.out.println(grades);
+    }
+}
---
+++ src/hackerrank_solutions/SamGrades2.java
@@ -0,0 +1,25 @@
+package hackerrank_solutions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SamGrades2 {
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        for (int i = 0; i < grades.size(); i++) {
+            int grade = grades.get(i);
+
+            if (grade >= 38) {
+                int nextMultipleOf5 = grade + (5 - grade % 5);//Bir sonraki 5 'in katı
+                if(nextMultipleOf5 - grade < 3){ //fark 3'ten küçükse yuvarla
+                    grades.set(i, nextMultipleOf5);
+                }
+            }
+        }
+        return grades;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> grades = Arrays.asList(73, 67, 38, 33);
+        System.out.println(grades);
+    }
+}
