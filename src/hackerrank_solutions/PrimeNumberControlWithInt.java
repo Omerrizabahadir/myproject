2ad6e061ec8842c1ee451b0b47aab7ef8a124c2a
Omerrizabahadir <omrbahadir@gmail.com>
2025-03-04 15:50:59 +0300

    Prime Number Control with primitive type int

--- src/hackerrank_solutions/PrimeNumberControlWithInt.java
+++ src/hackerrank_solutions/PrimeNumberControlWithInt.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class PrimeNumberControl {
+public class PrimeNumberControlWithInt {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
