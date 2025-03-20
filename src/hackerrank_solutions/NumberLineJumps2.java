c2093f37f3eca97f2c57d8002b8c7305f13daa9a
Omerrizabahadir <omrbahadir@gmail.com>
2025-03-20 12:14:06 +0300

    HackerRank 2 Kangaroo's Number Line Jumps  solve

---
+++ src/hackerrank_solutions/NumberLineJumps2.java
@@ -0,0 +1,42 @@
+package hackerrank_solutions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumberLineJumps2 {
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        if(x1 < x2 && v1 <= v2){
+            return "NO";
+        }
+        if(x2 < x1 && v2 <= v1){
+            return "NO";
+        }
+        if((v1 - v2) != 0 && (x2 - x1) % (v1 - v2) == 0 && (x2 - x1) / (v1 - v2) >= 0){
+            return "YES";
+        }
+        return "NO";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int x1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int v1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int x2 = Integer.parseInt(firstMultipleInput[2]);
+
+        int v2 = Integer.parseInt(firstMultipleInput[3]);
+
+        String result = NumberLineJumps.kangaroo(x1, v1, x2, v2);
+
+        System.out.println(result);
+        bufferedReader.close();
+    }
+}
+
+
